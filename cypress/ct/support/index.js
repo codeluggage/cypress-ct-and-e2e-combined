df1d746702cafa5736884b6336cef77eb05e0b45
Matias Forbord <matias.forbord@gmail.com>
2021-06-28 23:43:27 +0800

    Set up component testing

---
+++ cypress/ct/support/index.js
@@ -0,0 +1,59 @@
+// ***********************************************************
+// This example support/index.js is processed and
+// loaded automatically before your test files.
+//
+// This is a great place to put global configuration and
+// behavior that modifies Cypress.
+//
+// You can change the location of this file or turn off
+// automatically serving support files with the
+// 'supportFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/configuration
+// ***********************************************************
+
+// Import commands.js using ES2015 syntax:
+import './commands'
+
+// Alternatively you can use CommonJS syntax:
+// require('./commands')
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/vue/css/core.css'
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/vue/css/normalize.css'
+import '@ionic/vue/css/structure.css'
+import '@ionic/vue/css/typography.css'
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/vue/css/padding.css'
+import '@ionic/vue/css/float-elements.css'
+import '@ionic/vue/css/text-alignment.css'
+import '@ionic/vue/css/text-transformation.css'
+import '@ionic/vue/css/flex-utils.css'
+import '@ionic/vue/css/display.css'
+
+/* Theme variables */
+import '../../../src/theme/variables.css'
+
+
+// /* Core CSS required for Ionic components to work properly */
+// import '../../../node_modules/@ionic/vue/css/core.css'
+
+// /* Basic CSS for apps built with Ionic */
+// import '../../../node_modules/@ionic/vue/css/normalize.css'
+// import '../../../node_modules/@ionic/vue/css/structure.css'
+// import '../../../node_modules/@ionic/vue/css/typography.css'
+
+// /* Optional CSS utils that can be commented out */
+// import '../../../node_modules/@ionic/vue/css/padding.css'
+// import '../../../node_modules/@ionic/vue/css/float-elements.css'
+// import '../../../node_modules/@ionic/vue/css/text-alignment.css'
+// import '../../../node_modules/@ionic/vue/css/text-transformation.css'
+// import '../../../node_modules/@ionic/vue/css/flex-utils.css'
+// import '../../../node_modules/@ionic/vue/css/display.css'
+
+// /* Theme variables */
+// import '../../../src/theme/variables.css'
