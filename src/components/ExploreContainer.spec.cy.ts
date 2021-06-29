fac62a03ffd4582578a734fcf3fb0387b37fc57f
Matias Forbord <matias.forbord@gmail.com>
2021-06-30 01:35:02 +0800

    Working CT but not working toggle

--- src/components/ExploreContainer.spec.cy.ts
+++
@@ -1,9 +0,0 @@
-
-import ExploreContainer from "./ExploreContainer.vue"
-
-
-describe('<ExploreContainer />', () => {
-    it('Playground', () => {
-        cy.mount(ExploreContainer)
-    })
-})
--- src/components/ExploreContainer.vue
+++ src/components/ExploreContainer.vue
@@ -1,43 +1,76 @@
 <template>
-  <div id="container">
-    <strong>{{ name }}</strong>
-    <p>Explore <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
-  </div>
+  <ion-content>
+    <ion-title>{{ name }}</ion-title>
+
+    <ion-item>
+      <ion-label>Toggle the thing</ion-label>
+      <ion-toggle
+        v-model="toggle"
+        :checked="toggle"
+        @ionChange="toggleThing"
+      >
+      </ion-toggle>
+    </ion-item>
+
+  </ion-content>
 </template>
 
 <script lang="ts">
+ import {
+   IonContent,
+   IonTitle,
+   IonLabel,
+   IonItem,
+   IonToggle,
+ } from '@ionic/vue'
+ import { defineComponent, ref } from 'vue'
 
-export default {
-  name: 'ExploreContainer',
-  props: {
-    name: String
-  }
-}
+ export default defineComponent({
+   name: 'RoutineDetails',
+   props: {
+     name: String,
+   },
+   components: {
+     IonContent,
+     IonTitle,
+     IonLabel,
+     IonItem,
+     IonToggle,
+   },
+   setup () {
+     const toggle = ref(false)
+
+     return {
+       toggle,
+       toggleThing: () => { toggle.value = !toggle.value }
+     }
+   }
+ })
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
+ #container {
+   text-align: center;
+   position: absolute;
+   left: 0;
+   right: 0;
+   top: 50%;
+   transform: translateY(-50%);
+ }
 
-#container strong {
-  font-size: 20px;
-  line-height: 26px;
-}
+ #container strong {
+   font-size: 20px;
+   line-height: 26px;
+ }
 
-#container p {
-  font-size: 16px;
-  line-height: 22px;
-  color: #8c8c8c;
-  margin: 0;
-}
+ #container p {
+   font-size: 16px;
+   line-height: 22px;
+   color: #8c8c8c;
+   margin: 0;
+ }
 
-#container a {
-  text-decoration: none;
-}
+ #container a {
+   text-decoration: none;
+ }
 </style>
