0b345667ae5a933185a345760f5af5c90b560125
Matias Forbord <matias.forbord@gmail.com>
2021-06-30 11:57:07 +0800

    Update e2e test to match

--- cypress/e2e/specs/test.js
+++ cypress/e2e/specs/test.js
@@ -3,6 +3,7 @@
 describe('My First Test', () => {
   it('Visits the app root url', () => {
     cy.visit('/')
-    cy.contains('ion-content', 'Tab 1 page')
+    cy.contains('ion-content', 'Tab 1')
+    cy.contains('ion-label', 'Toggle the thing!')
   })
 })
